16d565e621d216cbf7dc8830d6b4d2ac351cba6d
Zachary Gaylord-Everett <zach.j.everett@gmail.com>
2022-12-02 16:14:00 -0500

    front end skeleton, pre routing.

---
+++ vue/src/components/Breweries.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Breweries Will Go Here
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/views/BreweriesView.vue
@@ -0,0 +1,15 @@
+<template>
+  <breweries></breweries>
+</template>
+
+<script>
+import Breweries from '../components/Breweries.vue'
+export default {
+  components: { Breweries },
+
+}
+</script>
+
+<style>
+
+</style>
