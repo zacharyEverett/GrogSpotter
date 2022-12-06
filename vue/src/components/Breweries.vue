1d9ca30158bb18be2c4fa88aa1f8d207b6d862dc
Zachary Gaylord-Everett <zach.j.everett@gmail.com>
2022-12-06 07:39:31 -0500

    Made first back to front connection, breweries page now displays an unformatted list of breweries from the DB

--- vue/src/components/Breweries.vue
+++ vue/src/components/Breweries.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
+  <div id="breweriesList">
       Breweries Will Go Here
       <ul>
-        <li ></li>
+        <li v-for="brewery in this.breweries" v-bind:key="brewery.id">{{ brewery.breweryName }}</li>
       </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+data() {
+  return {
+    breweries: [],
+  }
+  },
+  created(){
+    this.createList();
+  },
+  methods:{
+    createList(){
+      axios.get("/breweries").then(returned => 
+      {returned.data.forEach(element => {
+        this.breweries.push(element);
+      });})
+    }
+  }
+  
 }
 </script>
 
