4367038031a936b3748bbfa0fcca440bf9340db9
Daniel Bonnett <dbonnett1147@gmail.com>
2022-12-12 15:26:18 -0500

    Can now leave beer reviews

--- java/src/main/java/com/techelevator/controller/reviews/AddReviewController.java
+++ java/src/main/java/com/techelevator/controller/reviews/AddReviewController.java
@@ -27,14 +27,14 @@ public class AddReviewController {
     JdbcReviewDao reviewDao;
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping()
+    @PostMapping("/addReview/brewery")
     public Review addBreweryReview(@RequestBody @Valid BreweryReviewDto reviewDto){
         Review createdBreweryReview = new Review();
         return reviewDao.addBreweryReview(reviewDto);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{beerID}")
+    @PostMapping("/addReview/beer")
     public Review addBeerReview(@RequestBody @Valid BeerReviewDto reviewDto){
         return reviewDao.addBeerReview(reviewDto);
     }
