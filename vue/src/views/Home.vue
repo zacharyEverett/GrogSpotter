1545a7fd9554f0aa841e09aba4194c2bd4a38e83
Zachary <zacharyrmathes@gmail.com>
2022-12-13 11:18:52 -0500

    Home Page Nav/Style nav bar added/styled links are all operational at appropriate times reordered button availability

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,30 +1,7 @@
 <template>
   <div class="home">
-    <div class="route">
-      <div>
-      <router-link
-        :to="{ name: 'registerBrewery' }"
-        v-if="this.$store.state.isBrewer == true"
-      >
-        <h4 class="button-78">Add a Brewery</h4>
-      </router-link>
-      </div>
-      <div>
-      <router-link
-        :to="{ name: 'addBeer' }"
-        v-if="this.$store.state.isBrewer == true"
-      >
-        <h4 class="button-78">Add a Beer</h4>
-      </router-link>
-      </div>
-      <div>
-      <router-link :to="{ name: 'search' }">
-        <h4 class="button-78">Search</h4>
-      </router-link>
-      </div>
-    </div>
-    <div id="description"></div>
     <cards></cards>
+    
   </div>
 </template>
 
